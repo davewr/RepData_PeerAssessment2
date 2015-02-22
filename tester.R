c5478d8015a421ca03fb394bb9f01f273cb8760f
David <davewr@rogers.com>
2015-02-22 11:49:16 -0500

    Storm Data  Feb 21, 2015 11:49 Final

--- tester.R
+++ tester.R
@@ -1,32 +1,30 @@
-# PeerAssess1
-# StormData
-
-setwd("J:/coursera/DataScience/RepResearch/RepData_PeerAssessment2")
-getwd()
+# Tester...
 
 library("lubridate")
 library("dplyr")
 library("ggplot2")
 library("gridExtra")
 
-sdURL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
+
+sdURL <- "http://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 destFile = "repdata_data_StormData.csv.bz2"
-download.file(sdURL, destFile, mode="wb")
+# Remove download for testing only...
+#download.file(sdURL, destFile, mode="wb")
 
 
 # This works
 sd <- read.table(destFile, header=T,sep=",", stringsAsFactors = F)
 sd2 <-sd[,c(1:31,37)]
 
+
 # Setup year and month factors...
 sd2$BGN_DATE <- mdy_hms(sd2$BGN_DATE) #, "%m/%d/%Y %h:%M:%S", tz=sd2$TIME_ZONE)
 sd2$year <- year(sd2$BGN_DATE)
 sd2$month <- month(sd2$BGN_DATE)
-
 g1y <- sd2
 groupyr <- factor(sd2$year)
 g1y <- data.frame(year_group=groupyr,sd2)
-max(g1y$year)
+# max(g1y$year)
 
 # -------------
 # EVENTS -
@@ -50,10 +48,17 @@ toptenfat <- gev_fat_sum[1:10,]
 
 toptenpropdmg <- gev_pd_sum[1:10,]
 
-#inflateURL <- "https://raw.githubusercontent.com/davewr/RepData_PeerAssessment2/master/InflationAve.csv"
-#destFile <- "InflationAve.csv"
-#download.file(inflateURL, destFile, mode="wb")
 
+sum_propdmg_yr <- summarise(group_by(g1y,year_group), propdmg = sum(PROPDMG))
+
+
+
+
+spdyr <- sum_propdmg_yr
+
+
+
+# ------------------------
 
 # ------------------------
 evinjbycat <- ggplot(topteninj, aes(y=injuries, x=evt_group )) + 
@@ -63,12 +68,12 @@ evinjbycat <- ggplot(topteninj, aes(y=injuries, x=evt_group )) +
     ggtitle("Injuries by Event Type") +
     theme_bw()  +
     theme( axis.text.x = element_text(angle=60, hjust=1),
-    panel.grid.major.y= element_line(color="grey60", linetype="solid"),
-    panel.grid.minor.y= element_line(color="grey60", linetype="dashed"),
-    panel.grid.major.x= element_line(),
-    plot.title=element_text(size=rel(2)),
-    axis.title.x  = element_text(angle=0, vjust=0.5, size=rel(1.5)),
-    axis.title.y  = element_text(angle=90, vjust=0.5, size=rel(1.5))
+           panel.grid.major.y= element_line(color="grey60", linetype="solid"),
+           panel.grid.minor.y= element_line(color="grey60", linetype="dashed"),
+           panel.grid.major.x= element_line(),
+           plot.title=element_text(size=rel(2)),
+           axis.title.x  = element_text(angle=0, vjust=0.5, size=rel(1.5)),
+           axis.title.y  = element_text(angle=90, vjust=0.5, size=rel(1.5))
     )
 
 evinjbycat            
@@ -90,68 +95,9 @@ evfatbycat <- ggplot(toptenfat, aes(y=fatalities, x=evt_group )) +
            axis.title.x  = element_text(angle=0, vjust=0.5, size=rel(1.5)),
            axis.title.y  = element_text(angle=90, vjust=0.5, size=rel(1.5))
            #plot.xlab =element_text(size=rel(1.5))
-           )
+    )
 
 evfatbycat 
-          
-
-# --------------------------------
-
-
-
-
-
-
-sum_fat_yr <- summarise(group_by(g1y,year_group), fatalities = sum(FATALITIES))
-sum_inj_yr <- summarise(group_by(g1y,year_group), injuries = sum(INJURIES))
-sum_propdmg_yr <- summarise(group_by(g1y,year_group), propdmg = sum(PROPDMG))
-
-#yrlabels = as.character(seq(1950,2011, by = 5))
-#yrlabels2 = as.character(seq(1,62, by = 1))
-
-#yrs = seq(1950,2011, by = 5)
-
-#
-# CPI Inflation Data
-#inflateURL <- "https://raw.githubusercontent.com/davewr/RepData_PeerAssessment2/master/InflationAve.csv"
-#destFile <- "InflationAve.csv"
-#download.file(inflateURL, destFile, mode="wb")
-#ia <- read.csv(destFile, header=T,sep=",", stringsAsFactors = F)
-
-
-#corrfactor <- ia[1,2]
-#ia$AveCorrFactor <- corrfactor / ia$Ave. 
-#names(ia)[2] <- "Avg"
-
-# Order CPI by year Ascending
-#ia <- ia[order(ia$Year),]
-#plot(ia$Avg ~ia$Year, main="Consumer Price Index vs Year")
-
-spdyr <- sum_propdmg_yr
-#merged <- merge(myIncomplete, spit, by.x="interval", by.y="group", 
-
-#mergedpd <- merge(spdyr, ia, by.x="year_group", by.y="Year")
-
-#mergedpd$pd_cpi <- mergedpd$propdmg *  mergedpd$AveCorrFactor
-
-# ---------------------------
-# spdtcm <- summarise(group_by(g1m, group), spd = sum(steps))
-# ---------------------------
-
-
-# ue <- unique(sd$EVTYPE)
-# tail(sd2)
-
-# maxpd <- max(sd2$PROPDMG)
-# maxinj <- max(sd2$INJURIES)
-# maxfatal <- max(sd2$FATALITIES)
-
-# x1 <- sd2[sd2$PROPDMG >= maxpd-1,]
-# x2 <- sd2[sd2$INJURIES >= maxinj-1,]
-# x3 <- sd2[sd2$FATALITIES >= maxfatal-1,]
-
-# evtable <- data.frame(table(sd2$EVTYP, sd2$year))
-# head (evtable, 20)
 
 
 
@@ -160,7 +106,6 @@ require(gridExtra)
 # Storm Damge $$$ GGPLOT2
 
 pd <- ggplot(spdyr, aes(y=propdmg  / 1000, x=year_group, group=1))
-#pd <- pd + geom_line(data=mergedpd, aes(x=year_group, y=pd_cpi/1000, group=1), col="green")
 pd <- pd+ geom_point() + stat_smooth(method = "loess") +
     labs(title = "Storm Damage per Year -- Dollars") +
     labs(x = "Year", y = "Injuries") +
@@ -197,28 +142,5 @@ grid.arrange(pd, evpdbycat , nrow=2, newpage=TRUE)
 
 
 
-
-
-
-
-
-
-#  *** NOT USED  ***
-
-# FATALITIES per Year
-pf <- ggplot(sum_fat_yr, aes(y=fatalities, x=year_group, group=1))
-pf  + geom_point() + stat_smooth(method = "lm") +
-    labs(title = "Storm Fatalities per Year") +
-    labs(x = "Year", y = "Fatalities") +
-    scale_x_discrete(breaks=seq(1950, 2011, 5))
-
-# Injuries per year
-# -----------------
-pi <- pi <- ggplot(sum_inj_yr, aes(y=injuries, x=year_group, group=1))
-pi  + geom_point() + stat_smooth(method = "lm") +
-    labs(title = "Storm Injuries per Year") +
-    labs(x = "Year", y = "Injuries") +
-    scale_x_discrete(breaks=seq(1950, 2011, 5))
-
-# --------------------------------------------------
+ # *************************************
 
